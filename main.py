9d3c7b3ca8a99626400f6492b7f282c633d88e04
Saachi Varma Y <saachivarma135@gmail.com>
2026-07-22 14:36:16 +0530

    Read text files from documents folder

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
 
 files = os.listdir(path="documents")
-for file in files:
-    if file.endswith(".txt"):
-        print(file)
+for filename in files:
+    if filename.endswith(".txt"):
+        path = os.path.join("documents", filename)
+        with open(path, "r") as file:
+            file_content = file.read()
+        print(f"----- {filename} -----")
+        print(file_content)
